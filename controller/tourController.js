2e81dff8b65259fdbd48a3098b5247945668b054
Manoj Kanojiya <manoj.softvan@gmail.com>
2020-12-30 23:16:39 +0530

    Lec-95, 96, 97, 98 Added Pagination, filtering, sorting

--- controller/tourController.js
+++ controller/tourController.js
@@ -8,7 +8,34 @@ exports.getAllTours = async (req, res) => {
 
         // const query = Tour.find().where('difficulty').equals('easy').where('duration').equals(5)
 
-        const query = Tour.find(queryObj);
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        let query = Tour.find(JSON.parse(queryStr));
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        if (req.query.fields) {
+            const sortBy = req.query.fields.split(',').join(' ');
+            query = query.select(sortBy);
+        }else {
+            query = query.select('-__v');
+        }
+
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        if(req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error('Page not exist')
+        }
+
+        query = query.skip(skip).limit(limit);
 
         const tours = await query;
         res.status(200).json({
