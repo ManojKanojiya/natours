ed1d5041d773ef9f04ec2d1311234954ae04e3e3
Manoj Kanojiya <manoj.softvan@gmail.com>
2020-12-30 09:50:16 +0530

    Lec: 88, 89, 90, 91 Added Tour CRUD

--- server.js
+++ server.js
@@ -11,31 +11,6 @@ mongoose.connect(DB, {
     useUnifiedTopology: true
 }).then(() => console.log("DB connected..."));
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
-    },
-    ratings: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-})
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-    name: 'The Forest Hiker',
-    price: 497
-});
-
-testTour.save().then((doc) => {console.log("Save doc ", doc)})
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`App is running on port ${port}...`);
