f2b5029d1a84a14385fb03da289675a07ebbef46
Manoj Kanojiya <manoj.softvan@gmail.com>
2021-01-04 18:59:04 +0530

    Lec-107, 108 added custom and built in validator

--- controller/tourController.js
+++ controller/tourController.js
@@ -43,7 +43,7 @@ exports.createTour = async (req, res) => {
     } catch (err) {
         res.status(400).json({
             status: 'fail',
-            messsage: 'Invalid data sent!'
+            messsage: err
         })
     }
 }
