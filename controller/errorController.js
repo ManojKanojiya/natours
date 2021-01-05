c2e893bf3ff75c61751c48c638450b6e5a051161
Manoj Kanojiya <manoj.softvan@gmail.com>
2021-01-05 14:54:33 +0530

    Lec-117 Handle error during dev and prod

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,9 +1,34 @@
+sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        error: err,
+        stack: err.stack,
+        status: err.status,
+        message: err.message
+    })
+}
+
+sendErrorProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+    } else {
+        console.error("Error ", err)
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong'
+        })
+    }
+}
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res)
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res)
+    }
 }
