3adeddaa196f0a6e765a23542a20da5126346939
Manoj Kanojiya <manoj.softvan@gmail.com>
2021-01-11 11:10:46 +0530

    Lec-163, 164 added me and authentication and authorization in API

--- controller/userController.js
+++ controller/userController.js
@@ -11,7 +11,10 @@ filterObj = (obj, ...allowedFields) => {
     return newObj;
 }
 
-
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id;
+    next();
+}
 
 
 exports.updateMe = catchAsync(async (req, res, next) => {
