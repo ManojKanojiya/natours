25eadaffeaf0be06c421790135650dee566815a5
Manoj Kanojiya <manoj.softvan@gmail.com>
2021-01-11 12:56:12 +0530

    Lec-165, 166 Added import dev data and added index in tour model

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 dotenv.config({ path: `./config.env` })
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB, {
@@ -13,10 +15,14 @@ mongoose.connect(DB, {
 }).then(() => console.log("DB connected..."));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 const importData = async () => {
     try {
-        await Tour.create(tours)
+        await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
         console.log('Data loaded Successfully...')
     } catch (err) {
         console.log(err)
@@ -26,7 +32,9 @@ const importData = async () => {
 
 const deleteData = async () => {
     try {
-        await Tour.deleteMany()
+        await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('Data deleted Successfully...')
     } catch (err) {
         console.log(err)
