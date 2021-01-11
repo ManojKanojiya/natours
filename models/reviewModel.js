fed29d792fc726cf6d10528b5d93acb195195a9e
Manoj Kanojiya <manoj.softvan@gmail.com>
2021-01-11 14:06:59 +0530

    Lec-167, 168 Added calculation of avgRating and nRating using statics method

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -44,6 +45,48 @@ reviewSchema.pre(/^find/, function (next) {
     })
     next();
 })
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRatings: { $sum: 1 },
+                avgRatings: { $avg: '$rating' }
+            }
+        }
+    ])
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsAverage: stats[0].avgRatings,
+            ratingsQuantity: stats[0].nRatings
+        })
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsAverage: 4.5,
+            ratingsQuantity: 0
+        })
+    }
+
+}
+
+reviewSchema.post('save', function () {
+    this.constructor.calcAverageRatings(this.tour);
+})
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.findOne();
+    next();
+})
+
+reviewSchema.post(/^findOneAnd/, async function () {
+    this.r.constructor.calcAverageRatings(this.r.tour)
+})
+
 
 const Review = mongoose.model('Review', reviewSchema);
 
