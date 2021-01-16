4e738211bbf4d851e9d8e2cd1dcee1651216e238
Manoj Kanojiya <manoj.softvan@gmail.com>
2021-01-16 17:02:00 +0530

    Lec-194, 195, 196 Added update User data and password

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ const limiter = rateLimit({
 })
 app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 app.use(mongoSanitize());
