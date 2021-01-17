d4a14bfdc0132b27512f9c532151de94707d503a
Manoj Kanojiya <manoj.softvan@gmail.com>
2021-01-17 07:53:44 +0530

    Lec-205, 206 Added HTML email send class

--- utils/email.js
+++ utils/email.js
@@ -1,33 +1,59 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    },
-    secureConnection: 'false',
-    tls: {
-        ciphers: 'SSLv3',
-        rejectUnauthorized: false
+module.exports = class Email {
+	constructor(user, url) {
+		this.to = user.email;
+		this.firstname = user.name.split(' ')[0];
+		this.url = url;
+		this.from = `Manoj Kanojiya <${process.env.EMAIL_FROM}>`
+	}
 
-    }
-  });
+	newTransport() {
+		if (process.env.NODE_ENV === 'production') {
+			return 1;
+		}
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Manoj Kanojiya <manojkanojiya.1998@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
+		return nodemailer.createTransport({
+			host: process.env.EMAIL_HOST,
+			port: process.env.EMAIL_PORT,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD
+			},
+			secureConnection: 'false',
+			tls: {
+				ciphers: 'SSLv3',
+				rejectUnauthorized: false
 
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
-};
+			}
+		})
+	}
 
-module.exports = sendEmail;
+	async send(template, subject) {
+
+		const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+			firstname: this.firstname,
+			url: this.url,
+			subject
+		});
+		const mailOptions = {
+			from: this.from,
+			to: this.to,
+			subject,
+			html,
+			text: htmlToText.fromString(html)
+			// html:
+		};
+
+		await this.newTransport().sendMail(mailOptions)
+	}
+
+
+	async sendWelcome() {
+		await this.send('welcome', 'Welcome to the Natours Family!')
+	}
+
+
+}
